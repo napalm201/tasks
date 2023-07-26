a6c1fca2ea40c28ed8f59ced80522e1a60554465
napalm201 <lenapetrova1905@gmail.com>
2023-07-26 10:30:51 +0300

    d

--- task_1/WDraw.cpp
+++ task_1/WDraw.cpp
@@ -50,11 +50,7 @@ void WDraw::translate(const Point2d& p1)
 {
 }
 
-void WDraw::drawSegment (const Point2d& p1, const Point2d& p2) {
-    printf("(%f, %f) - (%f, %f)\n", p1.x(), p1.y(), p2.x(), p2.y());
-}
 
-void WDraw::drawText(const char* text) {
-    printf("%s\n", text);
-}
+
+
 
